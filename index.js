01f67986b5428c4914c9eed0b1d9cecfc843ec4d
Luis Ernesto Guzmán del Angel <103284719+LEDGAngel@users.noreply.github.com>
2025-02-27 20:51:21 -0600

    cambios funcionales

--- index.js
+++ index.js
@@ -7,11 +7,12 @@ import loginRouter from "./routes/login.router.js"
 
 
 const app = express();
+app.use(express.json());
 
 app.use(indexRouter);
 app.use(itemsRouter);
 app.use(loginRouter);
-app.use(express.json());
+
 
 
 app.listen(5001, console.log("http://localhost:5001"));
