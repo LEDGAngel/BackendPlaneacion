e7948429a1e02e26a9fa10ee1d825196ac115fcd
Luis Ernesto Guzmán del Angel <103284719+LEDGAngel@users.noreply.github.com>
2025-03-06 12:46:35 -0600

    cambios db no relacional

--- index.js
+++ index.js
@@ -2,19 +2,22 @@ import "dotenv/config"
 import express from "express";
 import indexRouter from "./routes/index_router.js"
 import itemsRouter from "./routes/items.router.js"
+import itemsRouter2 from "./routes/items2.routes.js"
 import loginRouter from "./routes/login.router.js"
 import cors from "cors";
 import morgan from "morgan";
-
-
+import { connectDB } from "./utils/mongodb.js";
 
 const app = express();
+
+connectDB();
 
 app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(indexRouter);
 app.use(itemsRouter);
+app.use(itemsRouter2);
 app.use(loginRouter);
 
 
